d6f96b37882289d9717833dea2756441b2129e1f
Bash-n-Git <165792573+Bash-n-Git@users.noreply.github.com>
2024-10-18 22:51:01 +0530

    Update Footer.tsx

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="relative bg-gradient-to-r from-blue-900 via-gray-900 to-black text-white pb-10 mt-10">
+    <footer className="relative bg-gradient-to-r from-blue-900 via-gray-900 to-black text-white pb-10 mt-10 -mb-5">
    
-      <div className="container mx-auto px-5 flex flex-col md:flex-row justify-between mt-12">
+   <div className="container mx-auto px-5 flex flex-col md:flex-row justify-between items-start space-y-6 md:space-y-0 mt-8">
         
         <div className="mb-8 md:mb-0">
           <h3 className="text-orange-400 font-bold mb-3 text-2xl">Developers</h3>
@@ -19,19 +19,22 @@ const Footer: React.FC = () => {
           <h3 className="text-orange-400 font-bold mb-3 text-2xl">Contact Us</h3>
           <ul className="space-y-3">
             <li>
-              <p className="font-semibold">Thrupthi</p>
+              <span className="font-semibold pr-3">Thrupthi</span>
+              <span className="text-blue-400">+91 7975016134</span>
               <p>Outreach Head, Shaastra</p>
-              <p className="text-blue-400">+91 7975016134</p>
+              
             </li>
             <li>
-              <p className="font-semibold">Vansh</p>
+              <span className="font-semibold pr-3">Vansh</span>
+              <span className="text-blue-400">+91 7404769417</span>
               <p>Publicity Coordinator, Shaastra</p>
-              <p className="text-blue-400">+91 7404769417</p>
+              
             </li>
             <li>
-              <p className="font-semibold">Roshan</p>
+              <span className="font-semibold pr-3">Roshan</span>
+              <span className="text-blue-400">+91 9156450775</span>
               <p>Publicity Coordinator, Shaastra</p>
-              <p className="text-blue-400">+91 9156450775</p>
+              
             </li>
           </ul>
         </div>
@@ -62,7 +65,7 @@ const Footer: React.FC = () => {
             </button>
           </div> */}
 
-          <div className="flex space-x-4 mt-20">
+          <div className="flex space-x-4 mt-8">
             <a href="#" aria-label="Facebook" className="hover:text-blue-500 transition-colors duration-300">
               <i className="fab fa-facebook text-5xl"></i>
             </a>
