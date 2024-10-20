21850c00254f5b19bbc2ee938fdc3f05579b34ec
sathwikmerugu45 <137002539+sathwikmerugu45@users.noreply.github.com>
2024-10-20 21:42:35 +0530

    resolved errors in pacakge.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
-<<<<<<< HEAD
-import react from '@vitejs/plugin-react'
-=======
 import react from '@vitejs/plugin-react-swc'
->>>>>>> pranav
 
 // https://vitejs.dev/config/
 export default defineConfig({
