6a767f7b7cd608b05ad3521a5680555c18f5e68d
Deepti <chennaisunday@gmail.com>
2024-10-22 00:32:56 +0530

    sixth commit

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,18 +1,45 @@
+import React, { useState } from 'react';
 import arrow from '../assets/arrow.svg'; // Adjust the import path as necessary
-import './home.css'
+import './home.scss';
 
 const MyComponent: React.FC = () => {
+  const [heading, setHeading] = useState('Get Ahead Of The Competition');
+
+  const handleInput = (event: React.FormEvent<HTMLSpanElement>) => {
+    const text = event.currentTarget.innerText;
+    setHeading(text);
+  };
+
+  const scrollToSection = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div id="c1" className="flex justify-center items-center h-screen w-full relative">
-      <div className="flex flex-col w-full items-center">
-        <div>
+      <div className="flex flex-col w-full items-center">       
         <h1
-          className="text-7xl text-center font-arial-black font-bold"
+          data-heading={heading}
+          className="relative text-8xl uppercase font-normal text-center m-0"
         >
-          Get Ahead Of<br/> The Competition</h1></div>
-           
+          <span
+            data-heading={heading}
+            contentEditable
+            suppressContentEditableWarning
+            onInput={handleInput}
+            className="relative block outline-none"
+          >
+            Get Ahead Of<br /> The Competition
+          </span>
+        </h1>
         <div className="border h-1/12 w-1/12 mt-6 rounded-full border-black p-2">
-          <img className="arrow animate-bounceTwice" src={arrow} alt="arrow" />
+          <div className='animate-bounceTwice'>
+            <a className='hover:cursor-pointer' onClick={() => scrollToSection('subhead')}>
+              <img className="arrow" src={arrow} alt="arrow" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,27 +1,47 @@
 import logo from '../assets/ShaastraLogoWhite.jpg';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Navbar: React.FC = () => {
-    const scrollToSection = (id: string) => {
-        const element = document.getElementById(id);
-        if (element) {
-          element.scrollIntoView({ behavior: "smooth" });
-        }
-      };
-      
-    return(
-        <>
-        <div className="flex justify-between w-full h-20 font-bold bg-[#0a1128] sticky top-0 opacity-80 z-10">
-            <div className="h-full p-2 w-2/12"><img src={logo} className="h-full w-5/6"/></div>
-            <div className="-mt-2 text-2xl w-3/5 flex justify-between items-center h-full mr-12 text-slate-200">
-            <a onClick={() => scrollToSection('c1')} className='hover:cursor-pointer hover:underline'>Home</a>
-            <a onClick={() => scrollToSection('c2')} className='hover:cursor-pointer hover:underline'>About</a>
-            <a onClick={() => scrollToSection('c3')} className='hover:cursor-pointer hover:underline'>Sampark</a>
-            <a onClick={() => scrollToSection('c4')} className='hover:cursor-pointer hover:underline'>Shaastra</a>
-            <a onClick={() => scrollToSection('c5')} className='hover:cursor-pointer hover:underline'>Campus Ambassador</a>
-            </div>
-        </div>
-        </>
-    )
-}
+  // State to control navbar visibility
+  const [showNavbar, setShowNavbar] = useState<boolean>(true);
+  const [lastScrollY, setLastScrollY] = useState<number>(0);
+
+  // Smooth scrolling to a section
+  const scrollToSection = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  // Control navbar visibility on scroll
+  const controlNavbar = () => {
+    const currentScrollY = window.scrollY;
+    setShowNavbar(currentScrollY <= lastScrollY || currentScrollY < 100); // Show navbar if scrolling up or near the top
+    setLastScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar);
+    return () => {
+      window.removeEventListener('scroll', controlNavbar);
+    };
+  }, [lastScrollY]);
+
+  return (
+    <nav className={`flex justify-between w-full h-28 font-bold bg-[#0a1128] sticky top-0 opacity-80 z-10 transition-transform duration-300 ${showNavbar ? '' : 'transform -translate-y-full'}`}>
+      <div className="h-full pt-2 w-2/12">
+        <img src={logo} className="mt-1 h-full w-5/6" alt="Shaastra Logo" />
+      </div>
+      <div className="-mt-2 text-3xl w-3/5 flex justify-between items-center h-full mr-12 text-slate-200">
+        <a onClick={() => scrollToSection('c1')} className='hover:cursor-pointer hover:underline'>Home</a>
+        <a onClick={() => scrollToSection('c2')} className='hover:cursor-pointer hover:underline'>About</a>
+        <a onClick={() => scrollToSection('c3')} className='hover:cursor-pointer hover:underline'>Sampark</a>
+        <a onClick={() => scrollToSection('c4')} className='hover:cursor-pointer hover:underline'>Shaastra</a>
+        <a onClick={() => scrollToSection('c5')} className='hover:cursor-pointer hover:underline'>Campus Ambassador</a>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
